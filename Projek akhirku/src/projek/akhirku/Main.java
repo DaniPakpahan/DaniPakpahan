f0648663b1a74e5a280e5cec577951e87700bec3
Hello Dani here <135022446+designitid@users.noreply.github.com>
2024-05-19 04:39:11 +0700

    Add files via upload

---
+++ Projek akhirku/src/projek/akhirku/Main.java
@@ -0,0 +1,11 @@
+package projek.akhirku;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Jframe inJframe = new Jframe();
+        inJframe.setVisible(true);
+    }
+    
+}
